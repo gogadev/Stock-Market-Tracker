c984e9c9cd9572c70c8fb1d6f6e871f748c83955
GORDANA_RADIVOJEVIC <goga405@hotmail.com>
2020-05-05 15:13:08 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import Navbar from "./components/navbar/Navbar";
+import StockCard from "./components/stock-card/StockCard";
+
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <StockCard />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/config/iex.js
@@ -0,0 +1,4 @@
+export const iex = {
+  api_token: "pk_39f71a2f4d4943c4b6d09d2792a8be1d",
+  base_url: "https://cloud.iexapis.com/stable",
+};
